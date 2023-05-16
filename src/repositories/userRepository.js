a430cc574a6990a4d04276c7ca79e0d67db4b902
Casto Gil <casto782@gmail.com>
2023-05-16 01:38:04 -0300

    implementando logger

---
+++ src/repositories/userRepository.js
@@ -0,0 +1,37 @@
+import { userModel } from "../Dao/models/user.js";
+import EErrors from "../services/errors/enums.js";
+import CustomError from "../services/errors/CustomError.js";
+
+export const userRepository = {
+  getUserByEmail: async (email) => {
+    try {
+      return await userModel.findOne({ email: email });
+    } catch (error) {
+      throw new CustomError({
+        name: "Database Error",
+        cause: "Failed to get user by email",
+        code: EErrors.DATABASE_ERROR,
+        message:"Error in communication with repository database get User By Email "
+      })
+  }},
+  createUser: async (first_name, last_name, age, email, password, role) => {
+    try {
+      const user = new userModel({
+        first_name,
+        last_name,
+        age,
+        email,
+        password,
+        role,
+      });
+      return await user.save();
+    } catch (error) {
+      throw new CustomError({
+        name: "Database Error",
+        cause: "Failed to create User",
+        code: EErrors.DATABASE_ERROR,
+        message:"Error in communication with repository database create User "
+      })
+    
+  }}
+};
